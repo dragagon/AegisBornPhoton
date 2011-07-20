e57fadfd76c00056255668f37dfb2305945f65c9
Christian Richards <cjrgaming@gmail.com>
2011-07-20 16:10:06 -0700

    Added IAegisBornObject as base interface for all objects

--- AegisBornPhoton/AegisBorn/Models/Base/AegisBornObject.cs
+++ AegisBornPhoton/AegisBorn/Models/Base/AegisBornObject.cs
@@ -1,8 +1,12 @@
 ﻿namespace AegisBorn.Models.Base
 {
-    public class AegisBornObject
+    public class AegisBornObject : IAegisBornObject
     {
-        public int Id { get; set; }
+        public int Id
+        {
+            get { return ObjectId; }
+            set { ObjectId = value;  }
+        }
         public string Name { get; set; }
 
 
@@ -16,6 +20,74 @@
         public bool IsVisible
         {
             get { return _isVisible; }
+        }
+
+        public int InstanceId
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        public KnownObjectList KnownObjects
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public int ObjectId
+        {
+            get { throw new System.NotImplementedException(); }
+            private set { throw new System.NotImplementedException(); }
+        }
+
+        public int DbId
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public ObjectTransform Transform
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public void Spawn()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Spawn(int x, int y, int z)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Decay()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnSpawn()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RefreshId()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SendInfo(Actor.AegisBornPlayer player)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ToggleVisible()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
---
+++ AegisBornPhoton/AegisBorn/Models/Base/IAegisBornObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AegisBorn.Models.Base.Actor;
+
+namespace AegisBorn.Models.Base
+{
+    public interface IAegisBornObject
+    {
+        int InstanceId { get; set; }
+        bool IsVisible { get; }
+        KnownObjectList KnownObjects { get; }
+        string Name { get; set; }
+        int ObjectId { get; }
+        int DbId { get; }
+        ObjectTransform Transform { get; }
+
+        void Spawn();
+        void Spawn(int x, int y, int z);
+        void Decay();
+        void OnSpawn();
+        void RefreshId();
+        void SendInfo(AegisBornPlayer player);
+        void ToggleVisible();
+    }
+}
