c794c0740a93b1063d878a1fa3bb77f473391335
Christian Richards <cjrgaming@gmail.com>
2011-07-20 15:45:55 -0700

    Removing World and Region code

--- AegisBornPhoton/AegisBorn/Models/Base/AegisBornObject.cs
+++ AegisBornPhoton/AegisBorn/Models/Base/AegisBornObject.cs
@@ -4,22 +4,18 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+
+
         public float X { get; set; }
         public float Y { get; set; }
         public float Z { get; set; }
-
-        public virtual KnownObjectList KnownList { get; set; }
 
         private bool _isVisible;
 
         public bool IsVisible
         {
             get { return _isVisible; }
-        }
-
-        public AegisBornRegion WorldRegion
-        {
-            get { return null; }
         }
     }
 }
